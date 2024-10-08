8bf1f5f1fc39b28f1cf825054386f21ba18c9dc9
Ricky Primayuda Putra <113336156+rickyprimay@users.noreply.github.com>
2024-10-09 01:25:14 +0700

    Initial Commit

---
+++ HikeAdvanceApp/HikeAdvanceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeAdvanceAppApp.swift
+//  HikeAdvanceApp
+//
+//  Created by Ricky Primayuda Putra on 09/10/24.
+//
+
+import SwiftUI
+
+@main
+struct HikeAdvanceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
